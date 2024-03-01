dfea4347559fbc9ceb578445046c4f18c52393a1
amalcaraz <amalcaraz89@gmail.com>
2024-03-01 12:59:41 +0000

    Deploying to gh-pages from @ aleph-im/front-aleph-website-cms@fd56ff50a84f5994944473cbd75c1f6dbc477979 🚀

--- _next/static/yiNSHNyz4t1ryS9eUqp10/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3176f0d4f97a3f94.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

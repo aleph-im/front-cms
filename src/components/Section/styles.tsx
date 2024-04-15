f961f88f6dec84a75855653db8b3989164a6bc91
gmolki <gerardmo98@gmail.com>
2024-04-15 13:51:50 +0200

    fix: Section as <section> instead of <div>

--- src/components/Section/styles.tsx
+++ src/components/Section/styles.tsx
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 import { SectionProps } from "./types";
 import { calculateCssEditableProps } from "@/hooks/calculateCssEditableProps";
 
-export const StyledSection = styled.div<SectionProps>`
+export const StyledSection = styled.section<SectionProps>`
   ${({ theme, ...cssEditableProps }) => {
     return calculateCssEditableProps(theme, cssEditableProps);
   }}

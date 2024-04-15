ae2ca169fef3a6572c9917cfd90831b73bc32719
gmolki <gerardmo98@gmail.com>
2024-04-15 12:09:18 +0200

    Edit Symbol guard clause when no content

--- src/pages/edit-symbol.tsx
+++ src/pages/edit-symbol.tsx
@@ -8,11 +8,13 @@ builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 export const getStaticProps = async (context: { resolvedUrl: string }) => {
   // Dynamically fetch latest content from Builder.io API
   const content = await builder.get("symbol", { url: context.resolvedUrl });
-  console;
+
   return { props: { content } };
 };
 
 // View full integration and docs: https://builder.io/c/docs/developers
 export default function Page(props: { content: BuilderContent | undefined }) {
+  if (!props.content) return;
+
   return <BuilderComponent content={props.content} model="symbol" />;
 }

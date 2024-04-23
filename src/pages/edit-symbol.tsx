9cfa98f63f1b3d7ec5696bb7320dda543a130955
gmolki <gerardmo98@gmail.com>
2024-04-23 07:57:23 +0200

    fix: edit symbol page preview

--- src/pages/edit-symbol.tsx
+++ src/pages/edit-symbol.tsx
@@ -1,18 +1,9 @@
 import { BuilderComponent, builder } from "@builder.io/react";
-import { BuilderContent } from "@builder.io/sdk";
 import React from "react";
 import "../builder-registry";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-export const getStaticProps = async (context: { resolvedUrl: string }) => {
-  // Dynamically fetch latest content from Builder.io API
-  const content = await builder.get("symbol", { url: context.resolvedUrl });
-
-  return { props: { content: content || null } };
-};
-
-// View full integration and docs: https://builder.io/c/docs/developers
-export default function Page(props: { content: BuilderContent | undefined }) {
-  return <BuilderComponent content={props.content} model="symbol" />;
+export default function Page() {
+  return <BuilderComponent model="symbol" />;
 }

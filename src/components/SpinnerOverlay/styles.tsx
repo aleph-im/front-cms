40be7a0c498fb67e2f13a558ab3d12078cf75e36
gmolki <gerardmo98@gmail.com>
2024-04-29 11:31:19 +0200

    fix: smooth loading transtion

--- src/components/SpinnerOverlay/styles.tsx
+++ src/components/SpinnerOverlay/styles.tsx
@@ -1,23 +1,25 @@
-import tw from 'twin.macro'
-import styled, { css } from 'styled-components'
+import tw from "twin.macro";
+import styled, { css } from "styled-components";
 
 export type StyledSpinnerContainerProps = {
-  $show: boolean
-  $center: boolean
-  $fullScreen: boolean
-}
+  $show: boolean;
+  $center: boolean;
+  $fullScreen: boolean;
+};
 
 export const StyledSpinnerContainer = styled.div<StyledSpinnerContainerProps>`
   ${({ theme, $fullScreen, $show, $center }) => css`
     ${tw`inset-0 flex justify-center py-10 h-full min-h-[11rem]`}
-    position: ${$fullScreen ? 'fixed' : 'absolute'};
+    position: ${$fullScreen ? "fixed" : "absolute"};
     backdrop-filter: blur(3px);
     border-radius: inherit;
-    opacity: ${$show ? '1' : '0'};
-    align-items: ${$center ? 'center' : 'flex-start'};
-    padding-top: ${$center ? '2.5rem' : '6rem'};
+    opacity: ${$show ? "1" : "0"};
+    align-items: ${$center ? "center" : "flex-start"};
+    padding-top: ${$center ? "2.5rem" : "6rem"};
     z-index: 99;
-    transition: all ease-in-out 500ms 500ms;
+    transition: ${$show
+      ? "all ease-in-out 100ms 0ms"
+      : "all ease-in-out 500ms 0ms"};
     background-color: ${theme.color.contentBackground}0C;
   `}
-`
+`;

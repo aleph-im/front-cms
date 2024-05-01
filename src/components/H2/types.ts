7cf154d913ad520dcb04abfeed842fd12a21a851
gmolki <gerardmo98@gmail.com>
2024-05-01 17:29:33 +0200

    feat: add css options to H2

--- src/components/H2/types.ts
+++ src/components/H2/types.ts
@@ -1,9 +1,7 @@
 import { TextProps } from "@/types/TextProps";
 import { TypoKind } from "@aleph-front/core";
-import { HTMLAttributes } from "react";
 
-export type H2Props = HTMLAttributes<HTMLHeadingElement> &
-  TextProps & {
-    color?: string;
-    type?: TypoKind;
-  };
+export type H2Props = TextProps & {
+  color?: string;
+  type?: TypoKind;
+};

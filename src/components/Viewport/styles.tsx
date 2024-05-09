892ad7e7aa85c3c4a23a5966acb3164ecc015860
gmolki <gerardmo98@gmail.com>
2024-05-09 21:32:15 +0200

    fix: viewport for small screens

---
+++ src/components/Viewport/styles.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+export const StyledViewport = styled.div`
+  ${tw`h-screen w-screen overflow-hidden`}
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { GlobalStyles } from "@aleph-front/core";
 import { GlobalStylesOverride } from "@/styles/global";
 import { getSelectedThemeData } from "@/utils/getSelectedThemeData";
 import Head from "next/head";
+import Viewport from "@/components/Viewport";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
@@ -38,7 +39,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={selectedTheme}>
         <GlobalStyles />
         <GlobalStylesOverride />
-        <Component {...pageProps} />
+        <Viewport>
+          <Component {...pageProps} />
+        </Viewport>
       </ThemeProvider>
     </>
   );

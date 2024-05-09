9d348d2af1a898deaee1c6e17abaa9a71f821b75
gmolki <gerardmo98@gmail.com>
2024-05-09 10:15:40 +0200

    feat: add Blog Article Card component

---
+++ src/components/Blog/BlogArticleCard/types.ts
@@ -0,0 +1,10 @@
+export type BlogArticleCardProps = {
+  title: string;
+  headline: string;
+  description: string;
+  thumbnailImage?: string;
+  featureImage?: string;
+  category: string;
+  blogArticleUrl: string;
+  size?: "md" | "lg" | "xl" | "full" | "highlighted";
+};

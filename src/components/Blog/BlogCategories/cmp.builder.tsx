31d9a8ce19ac0c12a192ad207f9986bee9502bbf
gmolki <gerardmo98@gmail.com>
2024-05-10 12:23:49 +0200

    feat: add BlogCategories component

---
+++ src/components/Blog/BlogCategories/cmp.builder.tsx
@@ -0,0 +1,10 @@
+import { Builder } from "@builder.io/react";
+import { DEFAULT_PROPS } from "@/constants/builderProps";
+import BlogCategories from ".";
+
+Builder.registerComponent(BlogCategories, {
+  ...DEFAULT_PROPS,
+  name: "Blog - Categories",
+  image:
+    "https://cdn.builder.io/api/v1/image/assets%2Fd72545ec06f647d993e1349bf57ebd7f%2Ffd9c99abfd534bbf9ebc839eed5acfd1",
+});

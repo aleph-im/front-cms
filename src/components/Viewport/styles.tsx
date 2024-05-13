e192115e7782f7114d5d7eeafe43da9d474cb11c
gmolki <gerardmo98@gmail.com>
2024-05-13 13:10:50 +0200

    fix: styled viewport scroll

--- src/components/Viewport/styles.tsx
+++ src/components/Viewport/styles.tsx
@@ -2,5 +2,5 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 export const StyledViewport = styled.div`
-  ${tw`h-screen w-screen overflow-hidden`}
+  ${tw`h-full overflow-hidden`}
 `;

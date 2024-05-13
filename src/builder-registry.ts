bd22a17d5c4d9f2e4f40411ad002e27dead03814
gmolki <gerardmo98@gmail.com>
2024-05-13 12:21:03 +0200

    feat: add LatestBlogArticles component

--- src/builder-registry.ts
+++ src/builder-registry.ts
@@ -25,6 +25,7 @@ import "./components/Tag/cmp.builder";
 import "./components/BlockTag/cmp.builder";
 import "./components/Icon/cmp.builder";
 import "./components/Blog/BlogArticlesGrid/cmp.builder";
+import "./components/Blog/LatestBlogArticles/cmp.builder";
 import "./components/Blog/BlogCategories/cmp.builder";
 import "./components/Blog/BlogTags/cmp.builder";
 import "./components/hiddenFromEditor.builder";

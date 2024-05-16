0ce5786a2cae07f6c81551407481dc306867ba36
gmolki <gerardmo98@gmail.com>
2024-05-16 14:51:17 +0200

    feat: add BlogArticlesGrid component

---
+++ src/components/Blog/BlogArticlesGrid/styles.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledBlogArticlesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 24px;
+  margin-bottom: 32px;
+`;

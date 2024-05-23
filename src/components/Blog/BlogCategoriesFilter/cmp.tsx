771b8d5ee431c3d930973cf4ec3b113142fc995b
gmolki <gerardmo98@gmail.com>
2024-05-23 18:26:09 +0200

    feat: add BlogCategoriesFilter component

---
+++ src/components/Blog/BlogCategoriesFilter/cmp.tsx
@@ -0,0 +1,107 @@
+import useCachedData from "@/hooks/common/useCachedData";
+import { BlogCategoryProps } from "@/types/blog/BlogCategoryProps";
+import { fetchAllCategories } from "@/utils/blog/fetchAllCategories";
+import { Button, Icon, Tag } from "@aleph-front/core";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import React, { useCallback, useEffect, useState } from "react";
+
+const DEFAULT_SELECTED_CATEGORIES: string[] = [];
+
+export const BlogCategories = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const [selectedCategories, setSelectedCategories] = useState<string[]>(
+    DEFAULT_SELECTED_CATEGORIES
+  );
+
+  const { data: categories, loading } = useCachedData(
+    "blogCategories",
+    useCallback(fetchAllCategories, [])
+  );
+
+  const handleCategoryFilterClick = (categoryId: string) => {
+    const queryParams = new URLSearchParams(searchParams);
+    const includeCategories = new Set(
+      queryParams.get("includeCategories")?.split(",")
+    );
+
+    includeCategories.has(categoryId)
+      ? includeCategories.delete(categoryId)
+      : includeCategories.add(categoryId);
+
+    includeCategories.size === 0
+      ? queryParams.delete("includeCategories")
+      : queryParams.set(
+          "includeCategories",
+          Array.from(includeCategories).filter(Boolean).toString()
+        );
+
+    router.push({
+      pathname: pathname,
+      query: queryParams.toString(),
+    });
+  };
+
+  const handleRemoveFilters = () => {
+    const queryParams = new URLSearchParams(searchParams);
+    queryParams.delete("includeCategories");
+
+    router.push({
+      pathname: pathname,
+      query: queryParams.toString(),
+    });
+  };
+
+  useEffect(() => {
+    setSelectedCategories(
+      searchParams.get("includeCategories")?.split(",") ||
+        DEFAULT_SELECTED_CATEGORIES
+    );
+  }, [searchParams]);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <div tw="flex flex-wrap items-start gap-x-2 gap-y-4 mb-4">
+        {categories.map((category: BlogCategoryProps) => {
+          const isSelected = selectedCategories.includes(category.id);
+
+          return (
+            <button
+              key={`${category.id}-${crypto.randomUUID()}`}
+              onClick={() => handleCategoryFilterClick(category.id)}
+            >
+              <Tag
+                variant={isSelected ? "accent" : "default"}
+                tw="flex justify-center items-center text-center"
+              >
+                <p
+                  className={`tp-body1 fs-18 ${
+                    isSelected ? "text-base1" : "text-base2"
+                  }`}
+                >
+                  {category.displayName}
+                </p>
+                {isSelected && <Icon tw="ml-3" name="xmark" size="lg" />}
+              </Tag>
+            </button>
+          );
+        })}
+      </div>
+      <Button
+        variant="textOnly"
+        as="button"
+        size="sm"
+        onClick={() => handleRemoveFilters()}
+      >
+        Clear all filters
+        <Icon tw="ml-1" size="lg" name="trash-can-xmark" />
+      </Button>
+    </>
+  );
+};
+
+export default BlogCategories;

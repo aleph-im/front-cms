55bea3f85a086b49d61a437a9d7397a5c8975c8a
gmolki <gerardmo98@gmail.com>
2024-05-30 12:22:02 +0200

    refactor: use dynamic article images

--- src/constants/blog.tsx
+++ src/constants/blog.tsx
@@ -2,7 +2,7 @@ import { GetContentOptions } from "@builder.io/sdk";
 
 export const DEFAULT_BUILDER_REQUEST_OPTIONS: GetContentOptions = {
   fields:
-    "data.title,data.description,data.headline,data.image,data.url,data.category,data.tags",
+    "data.title,data.description,data.headline,data.thumbnailImage,data.featureImage,data.url,data.category,data.tags",
   sort: { firstPublished: -1 },
   options: { noTargeting: true, includeRefs: true },
 };

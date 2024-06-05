9266c5604899017711c2b8b30e49a51246ef7e31
gmolki <gerardmo98@gmail.com>
2024-06-05 09:28:30 +0200

    refactor: move page metadata headers to its own file

--- src/pages/_dynamicPage.tsx
+++ src/pages/_dynamicPage.tsx
@@ -1,37 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import DefaultErrorPage from "next/error";
-import Head from "next/head";
 import { BuilderContent } from "@builder.io/sdk";
-import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
+import { BuilderComponent, useIsPreviewing } from "@builder.io/react";
 import { useRouter } from "next/router";
 import "../builder-registry";
 import StickyComponent from "@/components/StickyComponent";
 import { Loading } from "./_loading";
 import MainContentContainer from "@/components/MainContentContainer";
 import { useFetchBuilderContent } from "@/hooks/useFetchBuilderContent";
-
-type PageMetadataField = string | null;
-type PageOpenGraphProps = {
-  title?: PageMetadataField;
-  description?: PageMetadataField;
-  image?: PageMetadataField;
-  url?: PageMetadataField;
-};
-type PageTwitterProps = {
-  card?: PageMetadataField;
-  site?: PageMetadataField;
-  title?: PageMetadataField;
-  description?: PageMetadataField;
-  image?: PageMetadataField;
-};
-
-export type PageMetadataProps = {
-  title?: PageMetadataField;
-  description?: PageMetadataField;
-  canonicalUrl?: PageMetadataField;
-  openGraph?: PageOpenGraphProps | null;
-  twitter?: PageTwitterProps | null;
-};
+import PageMetadataHead, { PageMetadataProps } from "./_pageMetadataHead";
 
 export type DynamicPageProps = {
   page: BuilderContent | null;
@@ -77,84 +54,7 @@ export default function DynamicPage({
 
   return (
     <>
-      <Head>
-        <title>{pageMetadata?.title || content?.data?.metadata?.title}</title>
-        <meta
-          name="description"
-          content={
-            pageMetadata?.description || content?.data?.metadata?.description
-          }
-        />
-        <link
-          rel="canonical"
-          href={
-            pageMetadata?.canonicalUrl || content?.data?.metadata?.canonicalUrl
-          }
-        />
-        <meta
-          property="og:title"
-          content={
-            pageMetadata?.openGraph?.title ||
-            content?.data?.metadata?.openGraph?.title
-          }
-        />
-        <meta
-          property="og:description"
-          content={
-            pageMetadata?.openGraph?.description ||
-            content?.data?.metadata?.openGraph?.description
-          }
-        />
-        <meta
-          property="og:image"
-          content={
-            pageMetadata?.openGraph?.image ||
-            content?.data?.metadata?.openGraph?.image
-          }
-        />
-        <meta
-          property="og:url"
-          content={
-            pageMetadata?.openGraph?.url ||
-            content?.data?.metadata?.openGraph?.url
-          }
-        />
-        <meta
-          name="twitter:card"
-          content={
-            pageMetadata?.twitter?.card ||
-            content?.data?.metadata?.twitter?.card
-          }
-        />
-        <meta
-          name="twitter:site"
-          content={
-            pageMetadata?.twitter?.site ||
-            content?.data?.metadata?.twitter?.site
-          }
-        />
-        <meta
-          name="twitter:title"
-          content={
-            pageMetadata?.twitter?.title ||
-            content?.data?.metadata?.twitter?.title
-          }
-        />
-        <meta
-          name="twitter:description"
-          content={
-            pageMetadata?.twitter?.description ||
-            content?.data?.metadata?.twitter?.description
-          }
-        />
-        <meta
-          name="twitter:image"
-          content={
-            pageMetadata?.twitter?.image ||
-            content?.data?.metadata?.twitter?.image
-          }
-        />
-      </Head>
+      <PageMetadataHead pageMetadata={pageMetadata} content={content} />
       {content?.data?.header && (
         <StickyComponent placed_at="top">
           <BuilderComponent

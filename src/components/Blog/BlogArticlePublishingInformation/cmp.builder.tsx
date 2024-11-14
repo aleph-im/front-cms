06c7eab0c21a3b5d4ad1df3505b4374a5a375943
gmolki <gerardmo98@gmail.com>
2024-11-14 20:07:35 +0100

    feat: add article publishing information component

---
+++ src/components/Blog/BlogArticlePublishingInformation/cmp.builder.tsx
@@ -0,0 +1,20 @@
+import { Builder } from "@builder.io/react";
+import { DEFAULT_PROPS } from "@/constants/builderProps";
+import { CSS_EDITABLE_INPUTS_ADVANCED } from "@/constants/builderInputs";
+import BlogArticlePublishingInformation from "./cmp";
+
+Builder.registerComponent(BlogArticlePublishingInformation, {
+  ...DEFAULT_PROPS,
+  name: "Blog - Article Publishing Information",
+  image:
+    "https://cdn.builder.io/api/v1/image/assets%2Fd72545ec06f647d993e1349bf57ebd7f%2Ffd9c99abfd534bbf9ebc839eed5acfd1",
+  inputs: [
+    ...DEFAULT_PROPS.inputs,
+    {
+      name: "variant",
+      type: "string",
+      defaultValue: "readTime",
+      enum: ["readTime", "writtenBy"],
+    },
+  ],
+});

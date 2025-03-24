c126ecfc4f1294f32c7b8fa9f5bc880447f9a9f0
gmolki <gerardmo98@gmail.com>
2025-03-24 11:34:56 +0100

    fix(Section): effects not being applied

--- src/components/Section/cmp.tsx
+++ src/components/Section/cmp.tsx
@@ -5,9 +5,11 @@ import { SectionProps } from "./types";
 import { StyledSection } from "./styles";
 import { generateClassNames } from "@/utils/generateClassNames";
 
-export const Section = ({ children, effects, id, ...props }: SectionProps) => {
+export const Section = ({ children, id, ...props }: SectionProps) => {
   const classNames = generateClassNames(props);
 
+  console.log("classnmes", classNames);
+  console.log("props", props);
   return (
     <StyledSection
       className={classNames}
